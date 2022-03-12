14b032440f012ae0f660a351c34ce62d9f4a37a6
Pizzuti92 <80061006+Pizzuti92@users.noreply.github.com>
2022-03-12 15:37:46 +0100

    Add files via upload

--- script/script_runmam.sh
+++ script/script_runmam.sh
@@ -15,13 +15,15 @@ Help()
   echo "Or: sh scripts/script_Lib.sh [-d|-h]"
   echo "Options:"
   echo "-d | --directory [<directory_path>]: select the working directory"
+  echo "where the gomamposst_x.inp file is located."
+  echo "-t | --time: print time of execution of the code"
   echo "-h | --help: displays help"
   echo "***********************************"
 	
 }
 
 cartellacl=$PWD/
-
+printime=0
 while [ -n "$1" ]; do
   case $1 in
     -d | --directory)
@@ -30,14 +32,24 @@ while [ -n "$1" ]; do
     -h | --help) 
      Help
      exit;;
+    -t | --time)
+     printime=1 
+     ;;
   esac
   shift
 done
 
 
-
-
-
+start=`date +%s`
 ./gomamposstopt.e < $cartellacl\gomamposst_x.inp   
+end=`date +%s`
+
+runtime=$((end-start))
+
+if [ $printime -ge 1 ]; then
+    echo "******************************"
+	echo "time of execution: "  $runtime " seconds"
+	echo "******************************"
+fi	
 python3 plot.py               
 
